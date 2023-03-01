7566ac7b21e43cf379ca5421bdda2bfdb04e1d2f
ChaconneLuo <asgardannila@gmail.com>
2023-03-01 14:43:43 +0800

    Fixed offset.

--- src/canvas/Ikun/index.tsx
+++ src/canvas/Ikun/index.tsx
@@ -17,18 +17,20 @@ export default function useIkun({ HEIGHT = 600, WIDTH = 600, color = '#182562' }
   let ctx: CanvasRenderingContext2D;
 
   const imgContainer = useRef<HTMLDivElement | null>(null);
+  const imgHeight = 300,
+    imgWidth = 200;
   let animationFrameId: number;
 
-  const [dragToggle, setDragToggle] = useState<boolean>(false);
-  let togglePoint: Point = { x: WIDTH / 2, y: HEIGHT / 2 };
-
+  const [dragTrigger, setDragTrigger] = useState<boolean>(false);
+  const [triggerOffset, setTriggerOffset] = useState<Point>({ x: 0, y: 0 });
+  let triggerPoint: Point = { x: WIDTH / 2, y: HEIGHT / 2 };
   const offset = { x: 0, y: 0 };
 
   const updateOffset = () => {
     offset.x = canvas.current!.offsetLeft;
     offset.y = canvas.current!.offsetTop;
-    togglePoint.x = offset.x + WIDTH / 2;
-    togglePoint.y = offset.y + HEIGHT / 2;
+    triggerPoint.x = offset.x + WIDTH / 2;
+    triggerPoint.y = offset.y + HEIGHT / 2;
   };
 
   const init = (width: number, height: number) => {
@@ -44,7 +46,7 @@ export default function useIkun({ HEIGHT = 600, WIDTH = 600, color = '#182562' }
   };
 
   const initKunImage = () => {
-    const kunImage = new Image(197, 300);
+    const kunImage = new Image(imgWidth, imgHeight);
     kunImage.src = kun;
     imgContainer.current = document.createElement('div');
     imgContainer.current.style.position = 'absolute';
@@ -66,30 +68,30 @@ export default function useIkun({ HEIGHT = 600, WIDTH = 600, color = '#182562' }
     ctx.strokeStyle = color;
     ctx.lineWidth = 10;
     ctx.moveTo(point1.x, point1.y);
-    ctx.quadraticCurveTo(WIDTH / 2, HEIGHT - 100, point2.x - offset.x, point2.y - offset.y);
+    ctx.quadraticCurveTo(WIDTH / 2, HEIGHT - 100, point2.x - offset.x + triggerOffset.x, point2.y - offset.y + triggerOffset.y);
     ctx.stroke();
   };
   const draw = () => {
     ctx.clearRect(0, 0, WIDTH, HEIGHT);
-    drawLine({ x: WIDTH / 2, y: HEIGHT }, togglePoint);
-    console.log(togglePoint);
+    drawLine({ x: WIDTH / 2, y: HEIGHT }, triggerPoint);
   };
   const dragEventFunc = (e: MouseEvent) => {
     e.preventDefault();
-    togglePoint.x = e.pageX;
-    togglePoint.y = e.pageY;
-    setDragToggle(true);
+    triggerPoint.x = e.pageX;
+    triggerPoint.y = e.pageY;
+    setTriggerOffset({ x: WIDTH / 2 + offset.x - triggerPoint.x, y: HEIGHT / 2 + offset.y - triggerPoint.y });
+    setDragTrigger(true);
   };
   const moveFunc = (e: MouseEvent) => {
     e.preventDefault();
-    if (dragToggle) {
-      togglePoint.x = e.pageX;
-      togglePoint.y = e.pageY;
-      imgContainer.current!.style.transform = `translate3d(${togglePoint.x - WIDTH}px, ${togglePoint.y - HEIGHT + 100}px, 0)`;
+    if (dragTrigger) {
+      triggerPoint.x = e.pageX;
+      triggerPoint.y = e.pageY;
+      imgContainer.current!.style.transform = `translate3d(${triggerPoint.x - WIDTH / 2 - imgWidth / 2 - offset.x + triggerOffset.x}px, ${triggerPoint.y - HEIGHT / 2 - imgHeight / 2 - offset.y + triggerOffset.y}px, 0)`;
     }
   };
   const freeFunc = (e: MouseEvent) => {
-    setDragToggle(false);
+    setDragTrigger(false);
   };
   const initEvent = () => {
     imgContainer.current!.addEventListener('mousedown', dragEventFunc);
@@ -122,5 +124,5 @@ export default function useIkun({ HEIGHT = 600, WIDTH = 600, color = '#182562' }
       cancelAnimationFrame(animationFrameId);
       canvasContext.recover();
     };
-  }, [dragToggle]);
+  }, [dragTrigger, triggerOffset]);
 }
